ec1c3c95d2262118d68c8b354cfeeb6afe7f8f9f
Maria Gauna <maria-gauna@uiowa.edu>
2024-09-19 16:50:13 +0000

    Added Readme, changed client.py and server.py, added comments

--- A2/client.py
+++ A2/client.py
@@ -3,11 +3,13 @@ import sys
 import threading
 
 def listen_to_message(client_socket):
+    # function that loops continuosly until an exception occurs or (/quit) is typed,
+    # listening to messages from a server, prints out the message 
+    # and prompts user for a response
     while True:
         try:
             message = client_socket.recv(1024).decode('ascii')
             if message == "/quit" or not message: 
-                print("Disconnected")
                 break
             print(f'{message}')
             print("Reply in line below or type /quit to quit")
@@ -15,15 +17,14 @@ def listen_to_message(client_socket):
             print(f'Error recieving message: {e}')
             break
 
+# sends a message to a client_socket
 def send_message(message,client_socket):
-        try:
-            client_socket.send(message.encode('ascii'))
-        except Exception as e:
-            print(f'Error sending message: {e}')
+    client_socket.send(message.encode('ascii'))
 
 def main():
+    # Main that creates a new client_socket based on the command line input
     if len(sys.argv)!=3:
-        print("Input is wrong, type: python3 client.py <host> <port>")
+            print("Input is wrong, type: python3 client.py <host> <port>")
     else:
         host=str(sys.argv[1])
         port= int(sys.argv[2])
@@ -31,22 +32,29 @@ def main():
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         except socket.error as err: 
             print ("Socket creation failed with error %s" %(err))
-
+        
+        #connects that client to the server 
         try:
             client_socket.connect((host,port))
             print(f'The socket has been successfully connected to the server')
+                
         except socket.error as err:
-            print(f'Socket failed to connect to server with errror')
+            print(f'Failed to connect to server with errror')
             print(f'Please check port and host and try again')
+            sys.exit()
+         
+        # thread used to listen for messages from the server
         recieved_message = threading.Thread(target=listen_to_message, args=(client_socket, ))
         recieved_message.start()
+         
+        # loop that continuously prompts the user for an input 
+        # and then sends that input the server
         while True:
             print(f'Type /quit to leave chat or type your message on the line below')
             message = input()
             new_thread= threading.Thread(target = send_message,args=(message, client_socket,))
             new_thread.start()
             if message == "/quit" or not message:
-                print("You have left")
                 break     
         client_socket.close() 
         
--- A2/server.py
+++ A2/server.py
@@ -3,35 +3,42 @@ import sys
 from _thread import *
 import threading
 
+# opens output file and writes my HawkId and name 
 file = open('/home/mgauna/cs3640/A2/output.txt','w+')
 file.write('msgauna Maria Gauna \n') 
 file.write('\n')
 file.flush()
+
+#creates a set 
 set_of_clients = set()
 
+ 
 def client_thread(conn):
+    # adds a connection to a set.
     set_of_clients.add(conn)
     conn_name = conn.getpeername()[1]
 
+    # This thread then ends a loop waiting for messages from the connection and decodes it 
+    # Following instructions on how to quit ( removes that client from the set)
+    # Sends the connections message to all the clients in the set
+    # And prints on communications to output file and console
     while True: 
         data = conn.recv(1024).decode('ascii')
         if not data or data == '/quit':
             print(f'Goodbye. User [{conn_name}] has left chat')
-            file.write(f'\nUser [{conn_name}] has left the chat \n')
+            file.write(f'\nUser [{conn_name}] has left the chat\n')
             file.flush()
             set_of_clients.remove(conn)
             break
         else:
-            message_recv= f'User [{conn_name}]: {data}'
-            file.write(message_recv)
-            file.flush()
+            message_recv= f'\nUser [{conn_name}]: {data}'
             print(message_recv)
-            file.write(f'\n User [{conn_name}] sent [{data}]')
+            file.write(f'\n User [{conn_name}] sent: [{data}]\n')
             file.flush
             for c in set_of_clients:
                 if c != conn:
                     c_name= c.getpeername()[1]
-                    file.write(f'\n User [{c_name}] recieved [{data}] from User [{conn_name}]')
+                    file.write(f'\nUser [{c_name}] recieved message: [{data}] from User [{conn_name}]\n')
                     file.flush()
                     c.send(message_recv.encode("ascii"))
                     
@@ -39,29 +46,40 @@ def client_thread(conn):
             file.flush()
     conn.close()
 
-
+# Main that creates a socket from input in command line
+# Binds that socket to a host and port, and the listens on that port 
+# while accepting new clients and creates a new thread for each client
+# All while printing communications to an output file and the console
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if len(sys.argv)==2:
         port= int(sys.argv[1])
-        host= socket.gethostbyname("127.0.0.1")
+        try:
+            host= socket.gethostbyname("127.0.0.1")
+        except socket.gaierror: 
+            print("there was an error in resolving the host")
+            sys.exit()
          
         server_socket.bind((host,port))
         print(f'The socket has connected to {port}')
+        file.write(f'\nThe server has been created and connected to {port}\n')
+        file.flush()
         server_socket.listen(6) 
         print(f'{host} is listening on port: {port}') 
+        file.write(f'\nThe server is now listening.\n')
+        file.flush()
 
         while True:
             client,address = server_socket.accept()
-            file.write(f' \n User [{address[1]}] has joined \n')
+            file.write(f'\nUser [{address[1]}] has joined\n')
             file.flush()
-            print(f' User [{address[1]}] has joined ')
+            print(f'User [{address[1]}] has joined ')
             client = threading.Thread(target= client_thread ,args=(client, ))
             client.start()
     server_socket.close()      
 
-        
+#calls the main function
 if __name__=="__main__":
     main()  
-
+# closes the file
 file.close()
