24358b29a9fc21ab92de7dfa008322190357ac51
msgauna <maria-gauna@uiowa.edu>
2024-10-10 16:16:51 -0500

    adding A3 from group project work

--- A3/README.md
+++ A3/README.md
@@ -10,10 +10,12 @@
 In order to run this code, please ensure that you have Mininet and iPerf properly installed. <br> 
 You can check this by running: <br>
 <code>sudo mn --version</code> <br>
-<code>pip show iperf3</code> <br>
-All of the functionality needed to run iPerf tests on a Mininet object can be accessed from <code>network_bottleneck.py</code>, 
-you should not need to enter the <code>server.py</code> or <code>client.py</code> scripts. <br>
-Begin by opening a bash terminal and run the following command: <br>
+<code>pip show iperf3</code> <br> <br>
+All functionality needed to run iPerf tests on a Mininet object can be accessed from <code>network_bottleneck.py</code>, you should not need to enter the <code>server.py</code> or <code>client.py</code> scripts. <br> 
+<br> Begin by navigating to to A3 directory. <br> <br>
+The virtual environment used to create this code should be stored in the repository and can be activated with: <br>
+<code> source ~/cs3640/A3/bin/activate </code> <br>
+Open a bash terminal and run the following command: <br>
 <code>sudo mn -c</code> <br>
 This will ensure that the Mininet's network is clear and prevent errrors in creation and configuration of a new network.<br>
 Then, run the following command: <br>
@@ -25,12 +27,39 @@ Open a bash terminal and run: <br>
 Then, in a new bash terminal, run: <br>
 <code>python3 client.py -ip {client ip address} -port {port_number} -server_ip {ip address of server} -test {network protocol}</code> <br>
 <br>
-<em>Credit Reel:</em>
+You can run <code>analyze_perf.py</code> and get the network analysis in one of two ways. <br>
+(1) Press "Run Python File" in vscode <br>
+(2) Open a new bash terminal and run: <br>
+<code>python3 analyze_perf.py</code> <br> <br>
+<strong>Credit Reel:</strong> <br>
+<em>Madeline Harbaugh (mharbaugh) contributions:</em> <ul>
+<li> Completed Task 3:  Analyzing Network Performance 
+</ul>
+<em>Kristin To (kto) contributions: </em> <ul>
+<li> Completed Task 1:  Creating a network topology in Mininet
+</ul>
+<em>Elizabeth Elias (ecelias) contributions: </em> <ul>
+<li> Did part of Task 2:  Simulating Traffic with iPerf, including: <ul>
+<li> Installing iPerf3 and it’s Python bindings.
+<li> Wrote <code>run_perf_tests()</code>
+<li> Wrote the ReadMe Instructions
+</ul>
+</ul>
+<em>Maria Gauna (mgauna) contributions: </em> <ul>
+<li> Did part of Task 2: Simulating Traffic with iPerf, including: <ul>
+<li> Creating iPerf clients. 
+<li> Creating iPerf servers.
+<li> Wrote the ReadMe Credit Reel
+</ul>
+</ul>
+
 <ul>
 <li> [Mininet definitions and implementation](https://mininet.org/api/classmininet_1_1node_1_1Node.html)
 <li> [Plotting with Python](https://www.w3schools.com/python/matplotlib_pyplot.asp)
 <li> [iPerf implementation](https://iperf.fr/iperf-doc.php#3doc)
 <li> [iPerf implementation](https://iperf3-python.readthedocs.io/en/latest/modules.html)
 <li> [iPerf and Mininet Debugging](https://github.com/mininet/mininet/tree/master/examples)
-<li> [Anonymous Poet on this Piazza Post for iPerf Debugging](https://piazza.com/class/m03njulygss731/post/60)
+<li> [Anonymous Post on this Piazza Post for iPerf Debugging](https://piazza.com/class/m03njulygss731/post/60)
+<li> [Used this to find the default byte length of a packet](https://iperf.fr/iperf-doc.php#:~:text=read%20or%20write.-,iPerf%20works%20by%20writing%20an%20array%20of%20len%20bytes%20a,TCP%2C%201470%20bytes%20for%20UDP.)
+<li> [Implemention of mathplotlib](https://www.geeksforgeeks.org/data-visualization-using-matplotlib/)
 </ul>
--- A3/network_bottleneck.py
+++ A3/network_bottleneck.py
@@ -2,8 +2,7 @@
 from mininet.topo import Topo  
 from mininet.net import Mininet  
 from mininet.node import CPULimitedHost
-from mininet.node import Host, Controller, OVSController, OVSSwitch
-from mininet.cli import CLI
+from mininet.node import Host
 from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel   
@@ -12,8 +11,7 @@ import subprocess
 import json
 import time
 
-f = open("output-network-config.txt", "w+")
-
+net_config_file = open("output-network-config.txt", "w")
 
 class BottleneckTopo(Topo):
     #"Single switch connected to n hosts."  
@@ -69,12 +67,11 @@ def validateInput(validInt, bw_bottleneck, bw_other, time):
 def call_ifconfig(mininetObj):
     for host in mininetObj.hosts:
         host_file = open(f"output-ifconfig-{host}.txt", "w+")
+        # for each host in current mininet, call the ifconfig command
         current_host_config = host.cmd('ifconfig')
         host_file.write(current_host_config)
 
 # method to call the ping commands on all hosts in a mininet object and write the output to a file
-# ???? this method takes a while to run, maybe there's a way to speed it up
-# ???? i commented out this method call in the "run_topology_tests" for now bc it takes a decent chunk of time to run
 def call_ping(mininetObj):
     for src in mininetObj.hosts:
         host_file = open(f"output-ping-{src}.txt", "w+")
@@ -91,11 +88,9 @@ def call_ping(mininetObj):
 #need to add error handling if user doesn't input a valid int
 def run_topology_tests(bw_bottleneck, bw_other):
     #"Create and test a simple network"
-    # ???? not sure how to use BottleneckTopo.build() here but instructions say we should
     topo = BottleneckTopo(bw_bottleneck=bw_bottleneck, bw_other=bw_other)
 
-    # net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
-    # ???? modified for testing purposes because I didn't have cgroup v1 mounted and didn't want to do that right now
+    # Create the mininet object
     net = Mininet(topo=topo, host=Host, link=TCLink)
     net.start()
     print( "Dumping host connections" )
@@ -104,19 +99,19 @@ def run_topology_tests(bw_bottleneck, bw_other):
     # call the method to write the "ifconfig" command on all hosts to a file
     call_ifconfig(net)
 
-    # ???? commented out for now MAKE USRE TO UNCOMMENT BEFORE SUBMISSION
+    # call the method to write the "ping" command on all hosts to a file
     call_ping(net)
 
     # write details of hosts, switches, and links to output-network-config.txt file
     # ???? may need more info here?? I'm unsure of what "details" all need to be written
-    f.write("Nodes:\n")
+    net_config_file.write("Nodes:\n")
     for host in net.hosts:
-        f.write(f"  -Host: {host.name}\n")
+        net_config_file.write(f"  -Host: {host.name}\n")
     for switch in net.switches:
-        f.write(f"  -Switch: {switch.name}\n")
-    f.write("Links:\n")
+        net_config_file.write(f"  -Switch: {switch.name}\n")
+    net_config_file.write("Links:\n")
     for link in net.links:
-        f.write(f"  -Link: {link}\n")
+        net_config_file.write(f"  -Link: {link}\n")
 
     print( "Testing network connectivity" )
     net.pingAll()
@@ -125,6 +120,22 @@ def run_topology_tests(bw_bottleneck, bw_other):
     # ???? sorry couldn't figure out how to make this functional 
     #runTopOutput = subprocess.BottleneckTopo.build(bw_bottleneck, bw_other)
     #f.write(runTopOutput)
+
+# the output of the .pexec() funtion calling client.py is a string of everything printed to console
+# need to parse this string to remove \n and space characters
+#returns a list containing bytes sent and recv
+def tcp_bytes_sent_recv(tcp_out):
+    tcp_out = tcp_out.strip()
+    tcp_sent_recv = tcp_out.split()
+    return tcp_sent_recv
+
+# the output of the .pexec() funtion calling client.py is a string of everything printed to console
+# need to parse this string to remove \n and space characters
+#returns a list containing packets transmitted and packets lsot
+def udp_bytes_transmitted(udp_out):
+    udp_out = udp_out.strip()
+    udp_sent_recv = udp_out.split()
+    return udp_sent_recv
 
 # should be called by main
 def run_perf_tests(bw_bottleneck, bw_other):
@@ -146,42 +157,63 @@ def run_perf_tests(bw_bottleneck, bw_other):
     # initiliaze a tcp server on the h3 node
     tcp_server = h3.cmd('sudo python3 server.py -ip 10.0.0.3 -port 5201 &')
     # run the tcp test on the h1 node
-    tcp_test = h1.cmd('sudo python3 client.py -ip 10.0.0.1 -port 5201 -server_ip 10.0.0.3 -test tcp')
+    tcp_test = h1.pexec('sudo python3 client.py -ip 10.0.0.1 -port 5201 -server_ip 10.0.0.3 -test tcp')
+
+    # check if an error occured while creating the tcp test
+    if tcp_test[2] != 0:
+        print(f"An error occured. [Error code {tcp_test[2]}] {tcp_test[1]}")
+        return
+    
+    # parse the output of the tcp and return of list containing bytes sent and recieved
+    tcp_out = tcp_bytes_sent_recv(tcp_test[0])
 
     # configure info as dictionary to dump into json file for the tcp tests
-    # ???? this needs to be formatted correctly
     tcp_results = {
         "tcp_test": "tcp",
         "tcp_bottleneck_bw": bw_bottleneck, 
         "tcp_other_bw": bw_other, 
-        "tcp_bytes_sent": tcp_test,
-        "tcp_bytes_received": "please say this works"
+        "tcp_bytes_sent": tcp_out[0],
+        "tcp_bytes_received": tcp_out[1],
+        "tcp_seconds": tcp_out[2]
         }
     
     # dump test info into tcp json file
-    with open(f"output-tcp-{bw_bottleneck}-{bw_other}.json", "w") as tcp_file:
+    with open(f"output-tcp-{bw_bottleneck}-{bw_other}.json", "w+") as tcp_file:
             json.dump(tcp_results, tcp_file, indent=6)               
     
+    tcp_file.close()
     # initiliaze a udp server on the h4 node
     udp_server = h4.cmd('sudo python3 server.py -ip 10.0.0.4 -port 5202 &')
     # run the tcp test on the h1 node
-    udp_test = h2.cmd('sudo python3 client.py -ip 10.0.0.2 -port 5202 -server_ip 10.0.0.4 -test udp')
-    print(udp_test)
+    udp_test = h2.pexec('sudo python3 client.py -ip 10.0.0.2 -port 5202 -server_ip 10.0.0.4 -test udp')
+    
+    # check if an error occured while creating the udp test
+    if udp_test[2] != 0:
+        print(f"An error occured. [Error code {udp_test[2]}] {udp_test[1]}")
+        return
+    
+    # parse the output of the udp and return of list containing total packets sent and lost
+    udp_out = udp_bytes_transmitted(udp_test[0])
 
+    # calculate the number of bytes based on packets sent and lost
+    # assuming each packet = 1470 bytes
+    #bytes_sent = (int(udp_out[0]) * 1470)
+    #bytes_recv = (int(udp_out[0]) * 1470) - (int(udp_out[1]) * 1470)
+    
     # configure info as dictionary to dump into json file for the udp tests
-    # ???? this needs to be formatted correctly
     udp_results = {
         "udp_test": "udp",
         "udp_bottleneck_bw": bw_bottleneck, 
         "udp_other_bw": bw_other, 
-        "udp_bytes_sent": udp_test,
-        "udp_bytes_received": "please say this works"
+        "udp_bytes_transmitted": udp_out[0],
+        "udp_seconds": udp_out[1]
         }
     
     # dump test info into udp json files
-    with open(f"output-udp-{bw_bottleneck}-{bw_other}.json", "w") as udp_file:
+    with open(f"output-udp-{bw_bottleneck}-{bw_other}.json", "w+") as udp_file:
             json.dump(udp_results, udp_file, indent=6)               
 
+    udp_file.close()
 
 def main():
     #take in arguments
@@ -203,10 +235,10 @@ def main():
         if (bw_bottleneck) < (bw_other):
             if (isinstance(bw_bottleneck, int)) and (isinstance(bw_other, int) and isinstance(time, int)):
                 validInt = validateInput(validInt, bw_bottleneck, bw_other, time)
-    #run_topology_tests(bw_bottleneck, bw_other) 
+    run_topology_tests(bw_bottleneck, bw_other) 
     run_perf_tests(bw_bottleneck, bw_other)
 
 if __name__ == '__main__':
     main()
 
-f.close()
+net_config_file.close()
